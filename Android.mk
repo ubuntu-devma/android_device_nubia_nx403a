a54027f35b300c919342f7c1d986368af373112f
xenail-devma <xenial.devma@gmail.com>
2016-04-26 20:53:37 +0300

    modified:   Android.mk - to NX403A)

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter nx403a,$(TARGET_DEVICE)),)
+ifneq ($(filter NX403A,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
